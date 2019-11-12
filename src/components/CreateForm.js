89a79a105a26c7d433a815b9b38ec4f1418bb8ca
Lyndsey Duffield <lyndseyduffield1@gmail.com>
2019-11-11 16:27:10 -0800

    updated create form to use hooks

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,124 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import { addHabit } from "../actions";
 import { connect } from "react-redux";
 import "react-datepicker/dist/react-datepicker.css";
 
-class CreateForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-      goal: "",
-      startDate: new Date(),
-      endDate: new Date(),
-      accountabilityPartner: {
-        name: "",
-        email: ""
-      }
-    };
-  }
+const CreateForm = props => {
+  const initialState = {
+    title: "",
+    goal: "",
+    startDate: new Date(),
+    endDate: new Date(),
+    accountabilityPartner: {
+      name: "",
+      email: ""
+    }
+  };
 
-  handleChange = (key, value) => {
-    this.setState({
+  const [state, setState] = useState(initialState);
+
+  // "Methods"
+  const handleChange = (key, value) => {
+    setState({
+      ...state,
       [key]: value
     });
   };
 
-  handleAccountabilityPartnerChange = (key, value) => {
-    this.setState({
+  const handleAccountabilityPartnerChange = (key, value) => {
+    setState({
+      ...state,
       accountabilityPartner: {
-        ...this.state.accountabilityPartner,
+        ...state.accountabilityPartner,
         [key]: value
       }
     });
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const habit = {
-      title: this.state.title,
-      goal: this.state.goal,
-      startDate: this.state.startDate,
-      endDate: this.state.endDate,
+      title: state.title,
+      goal: state.goal,
+      startDate: state.startDate,
+      endDate: state.endDate,
       streak: [false],
-      accountabilityPartner: this.state.accountabilityPartner
+      accountabilityPartner: state.accountabilityPartner
     };
-    this.props.dispatch(addHabit(habit));
-    this.props.history.push("/");
+    props.dispatch(addHabit(habit));
+    props.history.push("/");
   };
 
-  render() {
-    return (
-      <div stye={{ textAlign: "center" }}>
-        <div>
-          <label>Habit</label>
-          <input
-            type="text"
-            placeholder="your new habit"
-            value={this.state.title}
-            onChange={event => {
-              this.handleChange("title", event.target.value);
-            }}
-          />
-          <label>Goal</label>
-          <input
-            type="text"
-            placeholder="why do you want to create this new habit?"
-            value={this.state.goal}
-            onChange={event => {
-              this.handleChange("goal", event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label>Start Date</label>
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={date => {
-              this.handleChange("startDate", date);
-            }}
-          />
-          <label>End Date</label>
-          <DatePicker
-            selected={this.state.endDate}
-            onChange={date => {
-              this.handleChange("endDate", date);
-            }}
-          />
-        </div>
-        <div>
-          <h3>Accountability Partner</h3>
-          <label>Name</label>
-          <input
-            type="text"
-            placeholder="their name"
-            value={this.state.accountabilityPartner.name}
-            onChange={event => {
-              this.handleAccountabilityPartnerChange(
-                "name",
-                event.target.value
-              );
-            }}
-          />
-          <input
-            type="text"
-            placeholder="their email"
-            value={this.state.accountabilityPartner.email}
-            onChange={event => {
-              this.handleAccountabilityPartnerChange(
-                "email",
-                event.target.value
-              );
-            }}
-          />
-        </div>
-        <button type="submit" onClick={this.handleSubmit}>
-          Submit
-        </button>
+  // Rendering
+  return (
+    <div stye={{ textAlign: "center" }}>
+      <div>
+        <label>Habit</label>
+        <input
+          type="text"
+          placeholder="your new habit"
+          value={state.title}
+          onChange={event => {
+            handleChange("title", event.target.value);
+          }}
+        />
+        <label>Goal</label>
+        <input
+          type="text"
+          placeholder="why do you want to create this new habit?"
+          value={state.goal}
+          onChange={event => {
+            handleChange("goal", event.target.value);
+          }}
+        />
       </div>
-    );
-  }
-}
+      <div>
+        <label>Start Date</label>
+        <DatePicker
+          selected={state.startDate}
+          onChange={date => {
+            handleChange("startDate", date);
+          }}
+        />
+        <label>End Date</label>
+        <DatePicker
+          selected={state.endDate}
+          onChange={date => {
+            handleChange("endDate", date);
+          }}
+        />
+      </div>
+      <div>
+        <h3>Accountability Partner</h3>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="their name"
+          value={state.accountabilityPartner.name}
+          onChange={event => {
+            handleAccountabilityPartnerChange("name", event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="their email"
+          value={state.accountabilityPartner.email}
+          onChange={event => {
+            handleAccountabilityPartnerChange("email", event.target.value);
+          }}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
 export default connect()(CreateForm);
