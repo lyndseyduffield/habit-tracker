e2ef5675126d5ed35b1586797220a4712fdb9f39
Lyndsey Duffield <lyndseyduffield1@gmail.com>
2019-11-11 16:34:43 -0800

    updated Edit Form to use hooks

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,122 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import { editHabit } from "../actions";
 import { connect } from "react-redux";
 
-class EditForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: this.props.habit.title,
-      goal: this.props.habit.goal,
-      startDate: this.props.habit.startDate,
-      endDate: this.props.habit.endDate,
-      accountabilityPartner: this.props.habit.accountabilityPartner
-    };
-  }
+const EditForm = props => {
+  const initialState = {
+    title: props.habit.title,
+    goal: props.habit.goal,
+    startDate: props.habit.startDate,
+    endDate: props.habit.endDate,
+    accountabilityPartner: props.habit.accountabilityPartner
+  };
 
-  handleChange = (key, value) => {
-    this.setState({
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (key, value) => {
+    setState({
+      ...state,
       [key]: value
     });
   };
 
-  handleAccountabilityPartnerChange = (key, value) => {
-    this.setState({
+  const handleAccountabilityPartnerChange = (key, value) => {
+    setState({
+      ...state,
       accountabilityPartner: {
-        ...this.state.accountabilityPartner,
+        ...state.accountabilityPartner,
         [key]: value
       }
     });
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const id = this.props.match.params.id;
+    const id = props.match.params.id;
     const habit = {
-      ...this.props.habit,
-      title: this.state.title,
-      goal: this.state.goal,
-      startDate: this.state.startDate,
-      endDate: this.state.endDate,
-      accountabilityPartner: this.state.accountabilityPartner
+      ...props.habit,
+      title: state.title,
+      goal: state.goal,
+      startDate: state.startDate,
+      endDate: state.endDate,
+      accountabilityPartner: state.accountabilityPartner
     };
-    this.props.dispatch(editHabit(habit, id));
-    this.props.history.push("/");
+    props.dispatch(editHabit(habit, id));
+    props.history.push("/");
   };
 
-  render() {
-    return (
-      <div stye={{ textAlign: "center" }}>
-        <div>
-          <label>Habit</label>
-          <input
-            type="text"
-            placeholder="your new habit"
-            value={this.state.title}
-            onChange={event => {
-              this.handleChange("title", event.target.value);
-            }}
-          />
-          <label>Goal</label>
-          <input
-            type="text"
-            placeholder="why do you want to create this new habit?"
-            value={this.state.goal}
-            onChange={event => {
-              this.handleChange("goal", event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <label>Start Date</label>
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={date => {
-              this.handleChange("startDate", date);
-            }}
-          />
-          <label>End Date</label>
-          <DatePicker
-            selected={this.state.endDate}
-            onChange={date => {
-              this.handleChange("endDate", date);
-            }}
-          />
-        </div>
-        <div>
-          <h3>Accountability Partner</h3>
-          <label>Name</label>
-          <input
-            type="text"
-            placeholder="their name"
-            value={this.state.accountabilityPartner.name}
-            onChange={event => {
-              this.handleAccountabilityPartnerChange(
-                "name",
-                event.target.value
-              );
-            }}
-          />
-          <input
-            type="text"
-            placeholder="their email"
-            value={this.state.accountabilityPartner.email}
-            onChange={event => {
-              this.handleAccountabilityPartnerChange(
-                "email",
-                event.target.value
-              );
-            }}
-          />
-        </div>
-        <button type="submit" onClick={this.handleSubmit}>
-          Submit
-        </button>
+  return (
+    <div stye={{ textAlign: "center" }}>
+      <div>
+        <label>Habit</label>
+        <input
+          type="text"
+          placeholder="your new habit"
+          value={state.title}
+          onChange={event => {
+            handleChange("title", event.target.value);
+          }}
+        />
+        <label>Goal</label>
+        <input
+          type="text"
+          placeholder="why do you want to create this new habit?"
+          value={state.goal}
+          onChange={event => {
+            handleChange("goal", event.target.value);
+          }}
+        />
       </div>
-    );
-  }
-}
+      <div>
+        <label>Start Date</label>
+        <DatePicker
+          selected={state.startDate}
+          onChange={date => {
+            handleChange("startDate", date);
+          }}
+        />
+        <label>End Date</label>
+        <DatePicker
+          selected={state.endDate}
+          onChange={date => {
+            handleChange("endDate", date);
+          }}
+        />
+      </div>
+      <div>
+        <h3>Accountability Partner</h3>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="their name"
+          value={state.accountabilityPartner.name}
+          onChange={event => {
+            handleAccountabilityPartnerChange("name", event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="their email"
+          value={state.accountabilityPartner.email}
+          onChange={event => {
+            handleAccountabilityPartnerChange("email", event.target.value);
+          }}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
   return {
