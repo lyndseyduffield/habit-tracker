b65588d41297355ef8f69369fbf496af2e5f1e36
Lyndsey Duffield <lyndseyduffield@protonmail.com>
2019-12-13 13:17:48 -0800

    Updates

--- precache-manifest.c45fcfde06cfefec0354011e6b977222.js
+++ precache-manifest.c45fcfde06cfefec0354011e6b977222.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6df5ba5500cf6de0d9816ace492b65d",
+    "revision": "cf92b9b576caada77b029d45b602605c",
     "url": "/habit-tracker/index.html"
   },
   {
-    "revision": "9ce82c13acc9d826c9ad",
+    "revision": "399ef1031fb78e269bf3",
     "url": "/habit-tracker/static/css/2.ff9ea1f9.chunk.css"
   },
   {
-    "revision": "0e768f07e93d9f91a93f",
+    "revision": "457688ee8ca2b965fedb",
     "url": "/habit-tracker/static/css/main.aaf334fc.chunk.css"
   },
   {
-    "revision": "9ce82c13acc9d826c9ad",
-    "url": "/habit-tracker/static/js/2.e11a7702.chunk.js"
+    "revision": "399ef1031fb78e269bf3",
+    "url": "/habit-tracker/static/js/2.19687f9b.chunk.js"
   },
   {
-    "revision": "0e768f07e93d9f91a93f",
-    "url": "/habit-tracker/static/js/main.28dcfdca.chunk.js"
+    "revision": "457688ee8ca2b965fedb",
+    "url": "/habit-tracker/static/js/main.50efa922.chunk.js"
   },
   {
     "revision": "21b13162b6b8c003e34f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/habit-tracker/precache-manifest.26d6c0ff765e2d10d3abd3c0dbf8b096.js"
+  "/habit-tracker/precache-manifest.c45fcfde06cfefec0354011e6b977222.js"
 );
 
 self.addEventListener('message', (event) => {
